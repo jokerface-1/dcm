d9af37a5b7eae2a480da9a51b297d79afaa9a6fb
Ajay <73016139+jokerface-1@users.noreply.github.com>
2024-05-25 23:18:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function goin() {
+    window.location.href="facilitiex.html"
+
+}
+function goin2(){
+    window.location.href="benifits.html"
+}
+const lcat = document.getElementById('contain2')
+lcat.addEventListener('click', ()=>{
+    document.getElementById('trichy').style.opacity = 1
+})
